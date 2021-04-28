3c02f8d70dfa591f5ade0c6e2cb725870ee389ab
Diego Cardoso <diegocar448@hotmail.com>
2021-04-28 11:13:14 -0300

    RaisedButton atualmente deprecated e substituido por ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,33 @@ void main() => runApp(new PerguntaApp());
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual primeiro',
+      'Qual segundo',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Questões'),
         ),
-        body: Text("Teste"),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
