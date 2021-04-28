739786f77bd498e4c4bb7eb4c514fe350f8264a4
Diego Cardoso <diegocar448@hotmail.com>
2021-04-28 13:12:29 -0300

    treinando stateLess e stateFull

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(new PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final List<String> perguntas = [
+    final perguntas = [
       'Qual primeiro',
       'Qual segundo',
     ];
@@ -17,18 +24,18 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[0]),
+            Text(perguntas[perguntaSelecionada]),
             ElevatedButton(
               child: Text('Resposta 1'),
-              onPressed: null,
+              onPressed: responder,
             ),
             ElevatedButton(
               child: Text('Resposta 2'),
-              onPressed: null,
+              onPressed: responder,
             ),
             ElevatedButton(
               child: Text('Resposta 3'),
-              onPressed: null,
+              onPressed: responder,
             ),
           ],
         ),
